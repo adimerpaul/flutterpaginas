261093f6a1bc93aba56a9536d79b57a255806995
adimerpaul <adimer101@gmail.com>
2024-10-08 22:19:41 -0400

    primera version

---
+++ lib/pages/myhome.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Home'),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+      ),
+      body: const Center(
+        child: Text('Hello, World!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
